0f6c69b6603c45d29dc24604c9a647e61f7eb6fd
samarchm024 <chmcs2026.samar.gound@gmail.com>
2026-02-11 10:28:11 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const port=process.env.PORT || 3000
 // })
 app.use(cors(
      {
-          origin: 'http://localhost:5173'
+          // origin: 'http://localhost:5173'
      }
 ))
 app.use(express.json())
